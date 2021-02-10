0df4e43efb2687d7d0994d59e74b26f2af679d90
edsonlipa <edson.lipa@gmail.com>
2021-02-10 18:28:01 -0500

    images to README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Ravn-Challenge-V2-EdsonLipa
+##Setup
+- [x]  npm 6.14.9
+- [x]  "bootswatch": "^4.6.0",
+- [x]  "graphql": "^15.5.0",
+- [x]  "react": "^17.0.1",
+    
+##Running instructions
+
+```
+git clone 
+cd project
+npm start
+```
+##Project description.
+
+Gifs or screenshots of the working application.
+Any assumptions you made during the project if necessary. Additional information.
+Technologies used.
--- star-wars-app/src/variables/staticComponents.js
+++ star-wars-app/src/variables/staticComponents.js
@@ -3,7 +3,7 @@ export const LOADING = <div className="alert alert-dismissible alert-info mt-5 d
                         <h4 className="alert-heading">Loading </h4>
                         <div className="spinner-border ml-auto" role="status" aria-hidden="true"></div>
                         </div>
-                        
-export const ERROR = <div className="alert alert-dismissible alert-warning" >
+
+export const ERROR = <div className="alert alert-dismissible alert-warning mt-5" >
                         <h4 className="alert-heading">Failed to Load Data </h4>
                         </div>
